ca14a28d83e3b7f9fecc71c0c0c2d928fd8abb99
nesb01t <nesb01t@163.com>
2024-09-01 17:11:14 +0800

    update: about

--- components/section/about.vue
+++ components/section/about.vue
@@ -14,14 +14,34 @@ const hobbies = ['唱', '跳', 'rap', '篮球']
         <div>Yurina</div>
       </div>
 
-      <div class="text-lg">
-        你好，我是<br />
-        Yurina，喜欢唱，跳，rap，篮球，画画<br />
-        看书，旅行，摄影，健身，瑜伽，冥想，学习，思考
+      <div class="text-base flex flex-col gap-1">
+        <span> 你好 我是yurina 目前是一个动画专业的在校大学生 </span>
+
+        <span>
+          目前的制作方向是三维以及二维的动画和游戏，视频的剪辑以及后期。
+        </span>
+
+        <span> houdini特效正在学习中 ，RPGmaker像素类游戏制作中…， </span>
+
+        <span> 闲暇之时偶尔会写一些游戏剧本大纲，动画故事。 </span>
+        <span>
+          3D方向熟悉的软件：unreal engine5,maya,cinema4D,daz3D,marvelous
+          designer,substance 3d,Zbrush等</span
+        >
+
+        <span>
+          2D方向：sai,ps,live2d模型+绘画制作，ae制作动态CG以及后期,偶尔自制ai绘画模型（注明：仅用于学习参考
+        </span>
+
+        <span>
+          视觉传达方面会进行logo以及海报，交互界面的制作。 联系我-一起进步
+        </span>
+
+        <span> 邮箱：3317414319@qq.com 短信留言：（+86）15988192141 </span>
       </div>
     </div>
 
-    <div class="about-section__interest">
+    <div class="invisible about-section__interest">
       <div class="text-3xl">Hobbies</div>
       <div class="flex gap-4 flex-wrap">
         <div v-for="hobby in hobbies" :key="hobby" class="text-lg">
